674fc764a10743cbb6c2333c5d74f0c5d19c3910
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-12-05 11:51:58 +0100

    stade is working

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
   // INITIALIZE :
   useEffect(() => {
 
-
+    console.log("INITIALIZE")
 
     const initializeMediaStream = async () => {
       const localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
