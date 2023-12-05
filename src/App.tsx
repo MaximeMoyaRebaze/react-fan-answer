c4366e0eebe2386df621132dd6d4fdac9396d87c
Maxime <maxime#email.com>
2023-12-05 14:24:18 +0100

    code refactor ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,25 @@
 import './App.css'
 import React, { useEffect, useRef } from 'react';
-import io from 'socket.io-client'
-import { v4 as uuidv4 } from 'uuid';
+import * as Implementations from './Implementations'
 
 const App: React.FC = () => {
 
-  // TURN ICE SERVER CONFIG :
-  const configurationIceServer = {
-    iceServers: [
-      {
-        urls: [
-          'stun:stun1.l.google.com:19302',
-          'stun:stun2.l.google.com:19302',
-        ],
-      },
-    ],
-    iceCandidatePoolSize: 10,
-  };
-
-  // BACKEND :
   const serverUrlSocket = 'http://localhost:3001/fan'
+  // const serverUrlSocket = 'http://83.113.50.18:3001/fan'
 
-  // REF :
   const localVideoRef = useRef<HTMLVideoElement>(null);
 
-  // INITIALIZE :
   useEffect(() => {
 
-    console.log("INITIALIZE")
-
     const initializeMediaStream = async () => {
-      const localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-      if (localVideoRef.current) {
-        localVideoRef.current.srcObject = localStream;
-      }
-      const peerConnection = new RTCPeerConnection(configurationIceServer);
-      const cellphoneId = 'Cellphone_' + uuidv4()
-      const socket = io(serverUrlSocket);
-      socket.on('connect', () => {
-        console.log('SOCKET CONNECTED');
 
-      })
+      const localStream = await Implementations.createLocalStream(localVideoRef)
 
+      const socket = await Implementations.createSocketConnetion(serverUrlSocket)
 
-      socket.on('fan connected', async (data: { room: { offer: RTCSessionDescription }, candidates: RTCIceCandidate[] }) => {
+      const cellphoneId = Implementations.createCellphoneUUID()
 
-        console.log("SOCKET on fan connected) : ", data);
-
-        await peerConnection.setRemoteDescription(new RTCSessionDescription(data.room.offer));
-        data.candidates.map(
-          async caller => {
-            console.log(`Got new remote ICE candidate: ${JSON.stringify(caller)}`);
-            await peerConnection.addIceCandidate(new RTCIceCandidate(caller));
-          })
-
-        const answer = await peerConnection.createAnswer();
-        await peerConnection.setLocalDescription(answer);
-        const roomWithAnswer = {
-          answer: {
-            type: answer.type,
-            sdp: answer.sdp,
-          },
-        };
-        socket.emit('save room with answer', { room: { answer: roomWithAnswer.answer }, id: cellphoneId })
-
-      })
-
-      if (localStream) {
-        localStream.getTracks().forEach((track) => {
-          console.log("ADD_TRACK", track);
-          peerConnection.addTrack(track, localStream);
-        });
-      }
-
-      peerConnection.addEventListener('icecandidate', async (event: RTCPeerConnectionIceEvent) => {
-        if (event.candidate) {
-          socket.emit('save callee candidate', { candidate: event.candidate, id: cellphoneId })
-        } else {
-          console.log('ICE candidate gathering completed.');
-        }
-
-      });
+      Implementations.createPeerConnection(localStream, socket, cellphoneId)
 
     };
 
@@ -94,7 +33,6 @@ const App: React.FC = () => {
         <h1>FAN</h1>
         <h2>Local Video</h2>
         <video ref={localVideoRef} autoPlay playsInline muted />
-
       </div>
     </div>
   );
---
+++ src/Implementations.ts
@@ -0,0 +1,90 @@
+import io, { Socket } from 'socket.io-client'
+import { v4 as uuidv4 } from 'uuid';
+
+// TURN ICE SERVER CONFIG :
+const configurationIceServer = {
+    iceServers: [
+        {
+            urls: [
+                'stun:stun1.l.google.com:19302',
+                'stun:stun2.l.google.com:19302',
+            ],
+        },
+    ],
+    iceCandidatePoolSize: 10,
+};
+
+// LOCALSTREAM : 
+export async function createLocalStream(localVideoRef: React.RefObject<HTMLVideoElement>) {
+    const localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    if (localVideoRef.current) {
+        localVideoRef.current.srcObject = localStream;
+    }
+    return localStream
+}
+
+// CELLPHONE UUID :
+export function createCellphoneUUID() {
+    return 'Cellphone_' + uuidv4()
+}
+
+// SOCKET :
+export async function createSocketConnetion(serverUrlSocket: string) {
+    const socket = io(serverUrlSocket);
+    socket.on('connect', () => {
+        console.log('SOCKET CONNECTED');
+    })
+    return socket
+}
+
+// PEERCONNECTION :
+export function createPeerConnection(localStream: MediaStream, socket: Socket, cellphoneId: string) {
+
+    const peerConnection = new RTCPeerConnection(configurationIceServer);
+
+    // PEER CONNECTION EVENT LISTENER :
+    peerConnection.addEventListener('icecandidate', async (event: RTCPeerConnectionIceEvent) => {
+        if (event.candidate) {
+            socket.emit('save callee candidate', { candidate: event.candidate, id: cellphoneId })
+        } else {
+            console.log('ICE candidate gathering completed.');
+        }
+
+    });
+
+    // ADD LOCAL STREAM TRACKS :
+    if (localStream) {
+        localStream.getTracks().forEach((track) => {
+            console.log("ADD_TRACK", track);
+            peerConnection.addTrack(track, localStream);
+        });
+    }
+
+    // SOCKET LISTENER :
+    socket.on('fan connected', async (data: { room: { offer: RTCSessionDescription }, candidates: RTCIceCandidate[] }) => {
+
+        console.log("SOCKET on fan connected) : ", data);
+
+        await peerConnection.setRemoteDescription(new RTCSessionDescription(data.room.offer));
+        data.candidates.map(
+            async caller => {
+
+                console.log(`Got new remote ICE candidate: ${JSON.stringify(caller)}`);
+
+                await peerConnection.addIceCandidate(new RTCIceCandidate(caller));
+            })
+        const answer = await peerConnection.createAnswer();
+        await peerConnection.setLocalDescription(answer);
+        const roomWithAnswer = {
+            answer: {
+                type: answer.type,
+                sdp: answer.sdp,
+            },
+        };
+
+        socket.emit('save room with answer', { room: { answer: roomWithAnswer.answer }, id: cellphoneId })
+
+    })
+
+    return peerConnection
+}
