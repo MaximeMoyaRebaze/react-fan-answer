05d83cd09d1adb58a15cb95d86977c75dc48b036
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-12-06 17:21:03 +0100

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import * as Implementations from './Implementations'
 export const App: React.FC = () => {
 
   // const serverUrlSocket = 'http://localhost:3001/fan'
-  const serverUrlSocket = 'http://83.113.50.18:3001/fan'
+  const serverUrlSocket = 'https://back-end-fan-cam-e765fca54737.herokuapp.com:34336/fan'
 
   const localVideoRef = useRef<HTMLVideoElement>(null);
 
