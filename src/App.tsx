541165010e77f881ba08249fb6bcd9008de8a3c8
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-12-08 09:34:08 +0100

    dprint

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
 import './App.css'
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react'
 
 import * as Implementations from './Implementations'
 
 export const App: React.FC = () => {
-
   // const serverUrlSocket = 'http://localhost:3001/fan'
   const serverUrlSocket = 'https://back-end-fan-cam-e765fca54737.herokuapp.com/fan'
 
-  const localVideoRef = useRef<HTMLVideoElement>(null);
+  const localVideoRef = useRef<HTMLVideoElement>(null)
 
   useEffect(() => {
-
-    // todo: check if the browser supports the WebRTC 
+    // todo: check if the browser supports the WebRTC
 
     const initializeMediaStream = async () => {
-
       const localStream = await Implementations.createLocalStream(localVideoRef)
 
       const socket = Implementations.createSocketConnection(serverUrlSocket)
 
-
-
       Implementations.createPeerConnection(localStream, socket)
+    }
 
-    };
-
-    initializeMediaStream();
-
-  }, []);
+    initializeMediaStream()
+  }, [])
 
   return (
     <div>
@@ -38,5 +31,5 @@ export const App: React.FC = () => {
         <video ref={localVideoRef} autoPlay playsInline muted />
       </div>
     </div>
-  );
-};
+  )
+}
